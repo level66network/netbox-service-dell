b1aff923a5d0b217479806dc779563058923eeef
root <root@netbox-1.fra1.de.companycloud.network>
2019-09-27 13:11:13 +0000

    Added new service types

--- functions.py
+++ functions.py
@@ -9,6 +9,8 @@ def dellServiceCode(code):
 	"""Maps the codes to short descriptions."""
 	if code == 'PO':
 		return 'Parts only'
+	elif code == 'KH':
+		return 'Keep Your Hard drive Service'
 	elif code == 'UD':
 		return 'VMware Workload'
 	elif code == 'RB':
@@ -29,6 +31,8 @@ def dellServiceCode(code):
 		return 'ProSupport Plus 4H'
 	elif code == '78':
 		return '8 Hour 7x24 Compellent Support'
+	elif code == '74':
+		return '4 Hour 7x24 Compellent Support'
 	else:
 		return ''
 
@@ -37,16 +41,19 @@ def dellCompareServiceCode(code1, code2):
 	weights = {
 		False: 0,
 		'UD': 0,
+		'KH': 0,
 		'PO': 10,
 		'RB': 15,
-		'NU': 20,
+		'NU': 45,
 		'NI': 20,
-		'PG': 30,
 		'SW': 30,
-		'ND': 40,
+		'ND': 30,
+		'PG': 40,
+		'NU': 45,
 		'78': 45,
 		'8I': 45,
 		'8U': 45,
+		'74': 50,
 		'4I': 50,
 		'4U': 50,
 		'PQ': 60
